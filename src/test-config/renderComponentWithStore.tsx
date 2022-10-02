088fa6e9d7a65c7537ea8717c3036349e6d71887
Darren Jaworski <darren.jaworski@e-hps.com>
2022-10-02 14:31:34 -0500

    add type help on render component with store

--- src/test-config/renderComponentWithStore.tsx
+++ src/test-config/renderComponentWithStore.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, RenderResult } from "@testing-library/react";
 import React from "react";
 import { Provider } from "react-redux";
 import { MockStoreEnhanced } from "redux-mock-store";
@@ -8,7 +8,7 @@ import { createDefaultStore } from "./fakeStores";
 export const renderComponentWithStore = (
   ComponentToRender: React.ReactElement,
   definedStore: MockStoreEnhanced<RootState> | null = null
-) => {
+): RenderResult => {
   const store = definedStore ? definedStore : createDefaultStore();
   return render(<Provider store={store}>{ComponentToRender}</Provider>);
 };
