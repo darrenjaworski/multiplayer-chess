0ea5b6eb93477443d5799f862ac6c1ca72733f25
Darren Jaworski <darren.jaworski@e-hps.com>
2022-10-02 12:17:31 -0500

    add formatting to test file

--- src/presentation/components/__tests__/Chessboard.test.tsx
+++ src/presentation/components/__tests__/Chessboard.test.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { render, screen, configure } from '@testing-library/react';
-import { Chessboard } from '../Chessboard';
+import { configure, render, screen } from "@testing-library/react";
+import { Chessboard } from "../Chessboard";
 
-describe('chessboard', ()=>{
-    it('renders chessboard on screen', ()=>{
-        configure({testIdAttribute: 'data-boardid'})
-        
-        const staticId = 12345;
-        render(<Chessboard id={staticId} />);
-        const chessboard = screen.getByTestId(staticId);
-        
-        expect(chessboard).toBeInTheDocument();
-    });
-})
+describe("chessboard", () => {
+  it("renders chessboard on screen", () => {
+    configure({ testIdAttribute: "data-boardid" });
+
+    const staticId = 12345;
+    render(<Chessboard id={staticId} />);
+    const chessboard = screen.getByTestId(staticId);
+
+    expect(chessboard).toBeInTheDocument();
+  });
+});
