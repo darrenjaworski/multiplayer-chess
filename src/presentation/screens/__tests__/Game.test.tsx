e4676cf4909b75530ccfb0ed3b85a3a729a352c1
Darren Jaworski <darren.jaworski@e-hps.com>
2022-10-10 22:56:45 -0500

    correct indexing on tests

--- src/presentation/screens/__tests__/Game.test.tsx
+++ src/presentation/screens/__tests__/Game.test.tsx
@@ -20,8 +20,8 @@ describe("game screen", () => {
     renderComponentWithStore(<Game />);
 
     const players = screen.getAllByTestId("player-name");
-    const p1Name = players[0];
-    const p2Name = players[1];
+    const p1Name = players[1];
+    const p2Name = players[0];
 
     expect(p1Name).toHaveTextContent(initialState.players[0].username);
     expect(p2Name).toHaveTextContent(initialState.players[1].username);
@@ -31,8 +31,8 @@ describe("game screen", () => {
     renderComponentWithStore(<Game />);
 
     const players = screen.getAllByTestId("player-ranking");
-    const p1Elo = players[0];
-    const p2Elo = players[1];
+    const p1Elo = players[1];
+    const p2Elo = players[0];
 
     expect(p1Elo).toHaveTextContent(String(initialState.players[0].eloScore));
     expect(p2Elo).toHaveTextContent(String(initialState.players[1].eloScore));
