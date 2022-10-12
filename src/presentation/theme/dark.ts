5d7ef5dcb1ed51dcbabf798f8f219b5e0ec9bfbb
Darren Jaworski <darren.jaworski@e-hps.com>
2022-10-11 19:51:53 -0500

    more theme updates

--- src/presentation/theme/dark.ts
+++ src/presentation/theme/dark.ts
@@ -3,7 +3,7 @@ import { Theme } from "./@types/Theme";
 // https://www.colourlovers.com/palette/4885822/%E0%B8%81%E2%8A%BE%E1%8E%A0uL%E2%82%A5uIzz
 export const darkTheme: Theme = {
   colors: {
-    primary: "blue",
+    primary: "#633D39",
     secondary: "#DBBBBF",
     text: "#B2CEB9",
     disabled: "#769491",
--- src/presentation/theme/light.ts
+++ src/presentation/theme/light.ts
@@ -3,8 +3,8 @@ import { Theme } from "./@types/Theme";
 // https://www.colourlovers.com/palette/4885813/%C7%9E%D0%B2Dvl%C9%B1vi11
 export const lightTheme: Theme = {
   colors: {
-    primary: "red",
-    secondary: "blue",
+    primary: "#CABFC3",
+    secondary: "#DBBBBF",
     text: "#455565",
     disabled: "gray",
   },
