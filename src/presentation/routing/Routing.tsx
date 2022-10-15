0cf0580095a34d58c8675e64d637276ddee242a4
Darren Jaworski <darrenjaworski@gmail.com>
2022-10-15 13:53:01 -0500

    handle gh pages

--- src/presentation/routing/Routing.tsx
+++ src/presentation/routing/Routing.tsx
@@ -9,30 +9,36 @@ import { Game, Player } from "../screens/Game";
 import { NotFound } from "../screens/NotFound";
 import { Start } from "../screens/Start";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Start />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/game",
-    element: <Game />,
-    action: async ({ request }) => {
-      // TODO this feels really janky. there has to be an easier way to do this!
-      const formData = await request.formData();
-      const playerOne = formData.get("playerOne");
-      const playertwo = formData.get("playerTwo");
-      const mode = Number(formData.get("mode")) as GameModes;
-
-      const players = [
-        { username: playerOne, eloScore: 1 },
-        { username: playertwo, eloScore: 2 },
-      ] as Player[];
-
-      store.dispatch(updatePlayers(players));
-      store.dispatch(updateMode(mode));
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Start />,
+      errorElement: <NotFound />,
     },
-    children: [{ path: ":id", element: <Game /> }],
-  },
-]);
+    {
+      path: "/game",
+      element: <Game />,
+      action: async ({ request }) => {
+        // TODO this feels really janky. there has to be an easier way to do this!
+        const formData = await request.formData();
+        const playerOne = formData.get("playerOne");
+        const playertwo = formData.get("playerTwo");
+        const mode = Number(formData.get("mode")) as GameModes;
+
+        const players = [
+          { username: playerOne, eloScore: 1 },
+          { username: playertwo, eloScore: 2 },
+        ] as Player[];
+
+        store.dispatch(updatePlayers(players));
+        store.dispatch(updateMode(mode));
+      },
+      children: [{ path: ":id", element: <Game /> }],
+    },
+  ],
+  {
+    basename:
+      process.env.REACT_APP_ENVIRONMENT === "GH" ? "/multiplayer-chess" : "/",
+  }
+);
