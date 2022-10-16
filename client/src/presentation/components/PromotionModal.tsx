1ac58b2a42a9ea2de72dc16112c781e982da5916
Darren Jaworski <darrenjaworski@gmail.com>
2022-10-16 14:21:25 -0500

    handle silented notifications on promotion. simplify icon logic

--- client/src/presentation/components/PromotionModal.tsx
+++ client/src/presentation/components/PromotionModal.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from "@emotion/react";
 import styled from "@emotion/styled";
 import { PieceSymbol } from "chess.js";
 import {
@@ -7,6 +8,8 @@ import {
   FaChessRook,
 } from "react-icons/fa";
 import useSound from "use-sound";
+import { useAppSelector } from "../../state-management/hooks";
+import { getShouldPlaySounds } from "../../state-management/slices/settings";
 import { piecePromotionSound } from "../soundEffects";
 import { Modal } from "./Modal";
 
@@ -23,42 +26,32 @@ interface PromotionModalProps {
 
 export const PromotionModal = (props: PromotionModalProps) => {
   const { handleClose, isOpen, promotePiece } = props;
-  const iconSize = "3rem";
+
   const [playPromotion] = useSound(piecePromotionSound);
+  const shouldPlaySounds = useAppSelector(getShouldPlaySounds);
+  const theme = useTheme();
+
+  const handlePromotion = (piece: "q" | "b" | "n" | "r") => {
+    if (shouldPlaySounds) playPromotion();
+    promotePiece(piece);
+  };
+
+  const iconSize = "3rem";
+
+  const iconProps = {
+    size: iconSize,
+    color: theme.colors.text,
+  };
 
   return (
     <Modal handleClose={handleClose} isOpen={isOpen}>
       <div data-testid="promotion-modal">
         <h2>Please select a piece for promotion:</h2>
         <PromotionPieces>
-          <FaChessQueen
-            size={iconSize}
-            onClick={() => {
-              playPromotion();
-              promotePiece("q");
-            }}
-          />
-          <FaChessBishop
-            size={iconSize}
-            onClick={() => {
-              playPromotion();
-              promotePiece("b");
-            }}
-          />
-          <FaChessKnight
-            size={iconSize}
-            onClick={() => {
-              playPromotion();
-              promotePiece("n");
-            }}
-          />
-          <FaChessRook
-            size={iconSize}
-            onClick={() => {
-              playPromotion();
-              promotePiece("r");
-            }}
-          />
+          <FaChessQueen {...iconProps} onClick={() => handlePromotion("q")} />
+          <FaChessBishop {...iconProps} onClick={() => handlePromotion("b")} />
+          <FaChessKnight {...iconProps} onClick={() => handlePromotion("n")} />
+          <FaChessRook {...iconProps} onClick={() => handlePromotion("r")} />
         </PromotionPieces>
       </div>
     </Modal>
