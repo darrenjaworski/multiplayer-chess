85ea5d1004966e7f18ec154aa0bf8b8047a3f972
Darren Jaworski <darren.jaworski@e-hps.com>
2022-10-20 09:11:16 -0500

    go back to passing state to begin with

--- client/src/state-management/appMiddleware.ts
+++ client/src/state-management/appMiddleware.ts
@@ -1,6 +1,5 @@
 import { CurriedGetDefaultMiddleware } from "@reduxjs/toolkit/dist/getDefaultMiddleware";
-import { gameSocketMiddleware } from "./middleware/GameSocketsMiddleware";
 
 export const appMiddleware = (
   getDefaultMiddleware: CurriedGetDefaultMiddleware
-) => getDefaultMiddleware().concat(gameSocketMiddleware);
+) => getDefaultMiddleware().concat();
